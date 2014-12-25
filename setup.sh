dd2aac7a09688a1e80b29c36f4c2181174878404
Keisuke Morita <k3suk3@gmail.com>
2014-12-25 19:16:16 +0900

    override directory link

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ DOT_FILES=(bin .ackrc .bash_profile .bashrc .inputrc .vimrc .gvimrc .vim)
 
 for file in ${DOT_FILES[@]}
 do
-    ln -sf $HOME/dotfiles/$file $HOME/$file
+    ln -sfn $HOME/dotfiles/$file $HOME/$file
 done
 
 [ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && echo "you should run following command to setup plugins ->  vim -c ':NeoBundleInstall'"
