ef5763e8b7e90b78d83409cce4d7a5c9887e41bd
Keisuke Morita <k3suk3@gmail.com>
2014-12-25 19:00:46 +0900

    add .ackrc

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DOT_FILES=(bin .bash_profile .bashrc .inputrc .vimrc .gvimrc .vim)
+DOT_FILES=(bin .ackrc .bash_profile .bashrc .inputrc .vimrc .gvimrc .vim)
 
 for file in ${DOT_FILES[@]}
 do
